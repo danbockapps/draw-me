cf76bff8a0a12c7e6902b372f204e768cc400cd5
Dan Bock <danbock@gmail.com>
2020-02-04 21:30:55 -0500

    Hide Firebase api key 🤫

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,21 +1,11 @@
 // Firebase App (the core Firebase SDK) is always required and must be listed first
-import * as firebase from 'firebase/app'
-
 // If you enabled Analytics in your project, add the Firebase SDK for Analytics
 import 'firebase/analytics'
-
+import * as firebase from 'firebase/app'
 // Add the Firebase products that you want to use
 import 'firebase/storage'
+import { firebaseOptions } from './secret'
 
-firebase.initializeApp({
-  apiKey: 'AIzaSyBB4KN97nfYsqDg5dLiYsnl68P-6Yju10k',
-  authDomain: 'danbock-draw-me.firebaseapp.com',
-  databaseURL: 'https://danbock-draw-me.firebaseio.com',
-  projectId: 'danbock-draw-me',
-  storageBucket: 'danbock-draw-me.appspot.com',
-  messagingSenderId: '916539086618',
-  appId: '1:916539086618:web:6ac1458ca7272db2b13ea3',
-  measurementId: 'G-Y18WQMMJ2P',
-})
+firebase.initializeApp(firebaseOptions)
 
 export const storage = firebase.storage()
